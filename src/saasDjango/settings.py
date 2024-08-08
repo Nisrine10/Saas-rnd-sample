2d3eafe27fc88faace29147053b21ba2372beb11
Nisrine10 <aomarinissrine2001@gmail.com>
2024-08-08 15:03:13 +0100

    changes of db

--- src/saasDjango/settings.py
+++ src/saasDjango/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
+from decouple import config
 from pathlib import Path
 
 
@@ -21,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nuueop%y%*ze16)%wk$1k3g_#6(!1d@qk0ojio=s1^)0q3@o%o'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = str(os.environ.get("DEBUG")).lower() == "true" 
+# DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true" 
+DEBUG=config("DJANGO_DEBUG", cast=bool)
 print("DEBUG", DEBUG,type(DEBUG))
 
 ALLOWED_HOSTS = [
@@ -90,6 +91,19 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
+CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30)
+
+DATABASE_URL = config("DATABASE_URL", cast=str)
+
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+    "default": dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=30,
+        conn_health_checks=True,
+                                      )
 }
 
 
