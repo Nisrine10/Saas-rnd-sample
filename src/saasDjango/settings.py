c0af2648d9495e4e09939741e8c50cf784f14e62
Nisrine10 <aomarinissrine2001@gmail.com>
2024-08-10 18:50:15 +0100

    Adding White Noise

--- src/saasDjango/settings.py
+++ src/saasDjango/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -156,8 +157,15 @@ STATICFILES_DIRS = [
 
 # output  for python manage.py collectstatic
 # local cdn
-STATIC_ROOT = BASE_DIR / "local-cdn"
 
+STATIC_ROOT = BASE_DIR / "local-cdn"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
