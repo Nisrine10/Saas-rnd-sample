4a94d3961e247cdf559f8eb961015605a1ff3b9c
Nisrine10 <aomarinissrine2001@gmail.com>
2024-08-29 17:35:58 +0100

    changed the saas theme

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+    './src/**/*.jsx',
+    // Add paths to your files here
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
